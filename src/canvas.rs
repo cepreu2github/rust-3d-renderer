cd39b364381e6426f6c8f87f99021d9122f3f089
Sergey Varyukhin <cepreu.mail@gmail.com>
2015-07-01 16:32:14 +1000

    [fighting with lifetimes]

--- src/canvas.rs
+++ src/canvas.rs
@@ -4,49 +4,66 @@ extern crate sdl2;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode;
+use sdl2::render::Renderer;
 
-pub fn test() {
-    let mut sdl_context = sdl2::init().video().unwrap();
+pub struct Canvas<'_> {
+    renderer: &'_ mut Renderer<'_>,
+}
 
-    let window = sdl_context.window("rust-sdl2 demo: Video", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+impl<'_> Canvas<'_> {
+    pub fn new(x: u32, y: u32) -> Canvas<'_> {
+        let mut sdl_context = sdl2::init().video().unwrap();
 
-    let mut renderer = window.renderer().build().unwrap();
+        let window = sdl_context.window("rust-3d-renderer", x, y)
+            .position_centered()
+            .opengl()
+            .build()
+            .unwrap();
 
-    // FIXME: rework it
-    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, (256, 256)).unwrap();
-    // Create a red-green gradient
-    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-        for y in (0..256) {
-            for x in (0..256) {
-                let offset = y*pitch + x*3;
-                buffer[offset + 0] = x as u8;
-                buffer[offset + 1] = y as u8;
-                buffer[offset + 2] = 0;
+        let mut renderer = window.renderer().build().unwrap();
+       
+        Canvas { renderer: &mut renderer }
+    }
+    
+    pub fn point(&self, x: u32, y: u32) {
+        // FIXME: rework it
+        let mut texture = self.renderer.create_texture_streaming(PixelFormatEnum::RGB24, (256, 256)).unwrap();
+        // Create a red-green gradient
+        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for y in (0..256) {
+                for x in (0..256) {
+                    let offset = y*pitch + x*3;
+                    buffer[offset + 0] = x as u8;
+                    buffer[offset + 1] = y as u8;
+                    buffer[offset + 2] = 0;
+                }
             }
-        }
-    }).unwrap();
+        }).unwrap();
 
-    renderer.clear();
-    renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 256, 256)));
-    renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(450, 100, 256, 256)), 30.0, None, (false, false));
-    renderer.present();
+        self.renderer.clear();
+        self.renderer.copy(&texture, None, Some(Rect::new_unwrap(100, 100, 256, 256)));
+        self.renderer.copy_ex(&texture, None, Some(Rect::new_unwrap(450, 100, 256, 256)), 30.0, None, (false, false));
+        self.renderer.present();
+    }
+    
+    pub fn wait_for_ESC(&self) {
+        let mut sdl_context = sdl2::init().video().unwrap();
 
-    let mut running = true;
+        let mut running = true;
 
-    while running {
-        for event in sdl_context.event_pump().poll_iter() {
-            use sdl2::event::Event;
+        while running {
+            for event in sdl_context.event_pump().poll_iter() {
+                use sdl2::event::Event;
 
-            match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    running = false
-                },
-                _ => {}
+                match event {
+                    Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                        running = false
+                    },
+                    _ => {}
+                }
             }
         }
     }
+
 }
+
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,7 @@ mod canvas;
 
 fn main() {
     println!("Hello World!");
-    canvas::test();
+    let canvas = canvas::Canvas::new(800, 600);
+    canvas.point(400, 300);
+    canvas.wait_for_ESC();
 }
