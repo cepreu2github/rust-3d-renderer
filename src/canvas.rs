37376e7711bd8a94503862e4bf91019b2e268fd8
Sergey Varyukhin <cepreu.mail@gmail.com>
2015-07-02 16:17:46 +1000

    line() complete.

--- src/canvas.rs
+++ src/canvas.rs
@@ -10,6 +10,8 @@ use sdl2::rect::Point;
 pub struct Canvas<'_> {
     renderer: Renderer<'_>,
     sdl_context: Sdl,
+    xsize: u32,
+    ysize: u32,
 }
 
 impl<'_> Canvas<'_> {
@@ -24,12 +26,17 @@ impl<'_> Canvas<'_> {
 
         let renderer = window.renderer().build().unwrap();
        
-        Canvas { renderer: renderer, sdl_context: sdl_context }
+        Canvas { 
+            renderer: renderer,
+            sdl_context: sdl_context,
+            xsize: x,
+            ysize: y,
+        }
     }
     
     pub fn set(&mut self, x: i32, y: i32, color: u32) {
         self.renderer.set_draw_color(Color::RGB((color >> (8*2)) as u8, (color >> (8*1)) as u8, color as u8));
-        self.renderer.draw_point(Point::new(x, y));
+        self.renderer.draw_point(Point::new(x, self.ysize as i32 - y));
         self.renderer.present();
     }
     
@@ -46,6 +53,56 @@ impl<'_> Canvas<'_> {
                         running = false
                     },
                     _ => {}
+                }
+            }
+        }
+    }
+
+    
+    // ============================================ BELOW IS PLATFORM INDEPENDENT CODE
+
+    pub fn line(&mut self, x1: i32, y1: i32, x2: i32, y2: i32, color: u32) {
+        if (x1 == x2) && (y1 == y2) {
+            self.set(x1, y1, color);
+        }
+        if (x1-x2).abs() > (y1-y2).abs() {
+            if x1 < x2 {
+                let mut yi = y1;
+                for xi in x1..x2+1 {
+                    let p = (y2-y1)*xi + x2*y1 - x1*y2;
+                    if yi*(x2-x1) < p + (y2-y1)/2 {
+                        yi = yi+1;
+                    }
+                    self.set(xi, yi, color);
+                }
+            } else {
+                let mut yi = y2;
+                for xi in x2..x1+1 {
+                    let p = (y1-y2)*xi + x1*y2 - x2*y1;
+                    if yi*(x1-x2) < p + (y1-y2)/2 {
+                        yi = yi+1;
+                    }
+                    self.set(xi, yi, color);
+                }
+            }
+        } else {
+            if y1 < y2 {
+                let mut xi = x1;
+                for yi in y1..y2+1 {
+                    let p = yi*(x2-x1) - x2*y1 + x1*y2;
+                    if xi*(y2-y1) < p + (y2-y1)/2 {
+                        xi = xi+1;
+                    }
+                    self.set(xi, yi, color);
+                }
+            } else {
+                let mut xi = x2;
+                for yi in y2..y1+1 {
+                    let p = yi*(x1-x2) - x1*y2 + x2*y1;
+                    if xi*(y1-y2) < p + (y1-y2)/2 {
+                        xi = xi+1;
+                    }
+                    self.set(xi, yi, color);
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 // Copyright (C) Cepreu <cepreu.mail@gmail.com> under GPLv2 and higher
 extern crate sdl2;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 mod canvas;
 
+const WHITE: u32 = 0xFFFFFF;
+const RED: u32 = 0xFF0000;
+const BLUE: u32 = 0x0000FF;
+
 fn main() {
-    println!("Hello World!");
-    let mut canvas = canvas::Canvas::new(800, 600);
-    canvas.set(400, 300, 0xFFFFFF);
+    env_logger::init().unwrap();
+    info!("starting up");
+    
+    let mut canvas = canvas::Canvas::new(100, 100);
+    canvas.line(13, 20, 80, 40, WHITE);
+    canvas.line(20, 13, 40, 80, RED);
+    canvas.line(80, 40, 13, 20, BLUE);
     canvas.wait_for_esc();
 }
