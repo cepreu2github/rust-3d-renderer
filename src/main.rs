59436c6c5fb111319aed86d945093d9dbef12ca4
Sergey Varyukhin <cepreu.mail@gmail.com>
2015-07-06 13:35:20 +1000

    Now we have test.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,14 @@ const WHITE: u32 = 0xFFFFFF;
 const RED: u32 = 0xFF0000;
 const BLUE: u32 = 0x0000FF;
 
+#[test]
+fn test_line() {
+    let mut canvas = canvas::Canvas::new(100, 100);
+    canvas.line(13, 20, 80, 40, WHITE);
+    canvas.line(20, 13, 40, 80, RED);
+    canvas.line(80, 40, 13, 20, BLUE);
+}
+
 fn main() {
     env_logger::init().unwrap();
     info!("starting up");
