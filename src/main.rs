3120a240a252d5f83b9e3053d4817c16f04337d9
Sergey Variukhin <cepreu.mail@gmail.com>
2015-07-10 07:38:16 +1000

    Restored after benchmarking again.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Copyright (C) Cepreu <cepreu.mail@gmail.com> under GPLv2 and higher
-//extern crate sdl2;
+extern crate sdl2;
 #[macro_use]
 extern crate log;
 extern crate env_logger;
@@ -13,8 +13,8 @@ use model::Model;
 const WHITE: u32 = 0xFFFFFF;
 //const RED: u32 = 0xFF0000;
 //const BLUE: u32 = 0x0000FF;
-const WIDTH: usize = 800;
-const HEIGHT: usize = 800;
+const WIDTH: usize = 700;
+const HEIGHT: usize = 700;
 
 #[test]
 fn test_line() {
@@ -27,27 +27,25 @@ fn test_line() {
 fn main() {
     env_logger::init().unwrap();
     info!("starting up");
-    for i in 0..100 {
-        let mut canvas = canvas::Canvas::new(WIDTH, HEIGHT);
-        let model = Model::new("african_head.obj");
-        debug!("drawing wireframe");
-        for face in model.faces {
-            debug!("processing face:");
-            debug!("({}, {}, {})", face[0], face[1], face[2]);
-            for j in 0..3 {
-                let v0 = &model.vertices[face[j] as usize];
-                let v1 = &model.vertices[face[(j+1)%3] as usize];
-                let x0 = ((v0.x+1.)*WIDTH as f32/2.) as i32;
-                let y0 = ((v0.y+1.)*HEIGHT as f32/2.) as i32;
-                let x1 = ((v1.x+1.)*WIDTH as f32/2.) as i32;
-                let y1 = ((v1.y+1.)*HEIGHT as f32/2.) as i32;
-                debug!("drawing line ({}, {}) - ({}, {})", x0, y0, x1, y1); 
-                canvas.line(x0, y0, x1, y1, WHITE);
-            }
+    let model = Model::new("african_head.obj");
+    let mut canvas = canvas::Canvas::new(WIDTH, HEIGHT);
+    debug!("drawing wireframe");
+    for face in model.faces {
+        debug!("processing face:");
+        debug!("({}, {}, {})", face[0], face[1], face[2]);
+        for j in 0..3 {
+            let v0 = &model.vertices[face[j] as usize];
+            let v1 = &model.vertices[face[(j+1)%3] as usize];
+            let x0 = ((v0.x+1.)*WIDTH as f32/2.) as i32;
+            let y0 = ((v0.y+1.)*HEIGHT as f32/2.) as i32;
+            let x1 = ((v1.x+1.)*WIDTH as f32/2.) as i32;
+            let y1 = ((v1.y+1.)*HEIGHT as f32/2.) as i32;
+            debug!("drawing line ({}, {}) - ({}, {})", x0, y0, x1, y1); 
+            canvas.line(x0, y0, x1, y1, WHITE);
         }
     }
     info!("drawing result");
-    //canvas.show();
+    canvas.show();
     info!("waiting for ESC");
-    //canvas.wait_for_esc();
+    canvas.wait_for_esc();
 }
