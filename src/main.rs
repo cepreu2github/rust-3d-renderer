0fa9a84939c89c3310c5974bd6add602bb78f38d
Sergey Varyukhin <cepreu.mail@gmail.com>
2015-07-10 16:37:03 +1000

    [working at triangle]

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,11 @@ mod model;
 use model::Model;
 
 const WHITE: u32 = 0xFFFFFF;
-//const RED: u32 = 0xFF0000;
+const RED: u32 = 0xFF0000;
 //const BLUE: u32 = 0x0000FF;
-const WIDTH: usize = 700;
-const HEIGHT: usize = 700;
+const GREEN: u32 = 0x00FF00;
+const WIDTH: usize = 200;
+const HEIGHT: usize = 200;
 
 #[test]
 fn test_line() {
@@ -29,21 +30,9 @@ fn main() {
     info!("starting up");
     let model = Model::new("african_head.obj");
     let mut canvas = canvas::Canvas::new(WIDTH, HEIGHT);
-    debug!("drawing wireframe");
-    for face in model.faces {
-        debug!("processing face:");
-        debug!("({}, {}, {})", face[0], face[1], face[2]);
-        for j in 0..3 {
-            let v0 = &model.vertices[face[j] as usize];
-            let v1 = &model.vertices[face[(j+1)%3] as usize];
-            let x0 = ((v0.x+1.)*WIDTH as f32/2.) as i32;
-            let y0 = ((v0.y+1.)*HEIGHT as f32/2.) as i32;
-            let x1 = ((v1.x+1.)*WIDTH as f32/2.) as i32;
-            let y1 = ((v1.y+1.)*HEIGHT as f32/2.) as i32;
-            debug!("drawing line ({}, {}) - ({}, {})", x0, y0, x1, y1); 
-            canvas.line(x0, y0, x1, y1, WHITE);
-        }
-    }
+    canvas.triangle(10, 70, 50, 160, 70, 80, RED);
+    canvas.triangle(180, 50, 150, 1, 70, 180, WHITE);
+    canvas.triangle(180, 150, 120, 160, 130, 180, GREEN);
     info!("drawing result");
     canvas.show();
     info!("waiting for ESC");
